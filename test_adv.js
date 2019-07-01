b245d64581cda0814c4cb40335edab5cee6aa0cd
Noriroll <norimitsu.shida@adroll.com>
2019-07-01 21:07:22 +0900

    Create test_adv.js

---
+++ test_adv.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+    var adroll_adv_id = "XCDEI7BTGNCZ7JXBNKPHPT";
+    var adroll_pix_id = "N6GOKSP4DZCUNIXPVBCOAW";
+	(function () {
+			var scr = document.createElement("script");
+			var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+			scr.setAttribute('async', 'true');
+			scr.type = "text/javascript";
+			scr.src = host + "/j/roundtrip.js";
+			((document.getElementsByTagName('head') || [null])[0] || 
+				document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+	}());
+</script>
